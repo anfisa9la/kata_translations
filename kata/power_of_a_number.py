9c40e2b8f656b24f3397a6b83f16df823e5e7317
Anfisa <anfisa.la@gmail.com>
2022-07-10 11:50:09 +0300

    add tests

--- kata/power_of_a_number.py
+++ kata/power_of_a_number.py
@@ -14,12 +14,18 @@ def greet(num, p):
 class TestPower(unittest.TestCase):
 
     def test_1(self):
+        self.assertEqual(power(2, 2), 4)
+        self.assertEqual(power(2, 3), 8)
+        self.assertEqual(power(2, 4), 16)
+        self.assertEqual(power(2, 5), 32)
+
+    def test_2(self):
         self.assertEqual(power(2, 5), 2 ** 5)
         self.assertEqual(power(3, 50), 3 ** 50)
         self.assertEqual(power(2, 200), 2 ** 200)
         self.assertEqual(power(200, 0), 1)
 
-    def test_2(self):
+    def test_3(self):
         self.assertEqual(power(2, -5), 2 ** -5)
         self.assertEqual(power(-3, 5), (-3) ** 5)
         self.assertEqual(power(2, -200), 2 ** -200)
